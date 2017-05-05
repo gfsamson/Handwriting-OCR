f86a79de9f984278f53aab6237671161e0284c60
gfsamson <gsamso1@lsu.edu>
2017-05-04 21:15:31 -0500

    updated svm code with images for testing and debugging

--- performRecognition.py
+++ performRecognition.py
@@ -8,7 +8,7 @@ import numpy as np
 clf = joblib.load("digits_cls.pkl")
 
 # Read the input image 
-im = cv2.imread("photo_3.jpg")
+im = cv2.imread("test_images/test_mom2.jpg")
 im = cv2.resize(im, (1080, 1080), interpolation=cv2.INTER_AREA)
 cv2.imshow("Original", im)
 cv2.waitKey(0)
@@ -24,13 +24,13 @@ im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
 #cv2.destroyAllWindows()
 
 # Threshold the image
-#ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
+#im_th = cv2.adaptiveThreshold(im_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,51,10)
+#ret, im_th = cv2.threshold(im_gray, 30, 255, cv2.THRESH_BINARY_INV)
 ret, im_th = cv2.threshold(im_gray, 60, 255, cv2.THRESH_BINARY_INV)
 cv2.imshow("Threshold Image", im_th)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
-#im_th = cv2.adaptiveThreshold(im_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,2)
 
 # Find contours in the image
 _, ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -45,6 +45,7 @@ for rect in rects:
     cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
     # Make the rectangular region around the digit
     leng = int(rect[3] * 1.6)
+    print('Length: {}'.format(leng))
     pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
     pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
     roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
@@ -54,14 +55,14 @@ for rect in rects:
     if roi.any():
         roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
         roi = cv2.dilate(roi, (3, 3))
-    # Calculate the HOG features
+        # Calculate the HOG features
         roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
         nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
         print('Prediction = {}'.format(nbr[0]))
     #cv2.imshow("Roi", roi)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
-        cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3, bottomLeftOrigin=1)
+        cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
 
 cv2.imshow("Resulting Image with Rectangular ROIs", im)
 cv2.waitKey(0)
